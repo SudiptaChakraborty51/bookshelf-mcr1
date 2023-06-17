7cb9c4bdcde89fa5e06b1c17ecd68732cdfa4044
Sudipta <hisudipta26@gmail.com>
2023-06-17 21:15:39 +0530

    updated books data

--- src/data.jsx
+++ src/data.jsx
@@ -3,83 +3,60 @@ export const booksData = [
     id: 1,
     title: "To Kill a Mockingbird",
     author: "Harper Lee",
-    publisher: "HarperCollins",
-    year: 1960,
     image:
       "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/41j-s9fHJcL.jpg",
-    price: 9.99,
-    category: "Read"
+    category: "Read",
   },
   {
     id: 2,
     title: "1984",
     author: "George Orwell",
-    publisher: "Penguin Books",
-    year: 1949,
     image: "https://m.media-amazon.com/images/I/71kxa1-0mfL.jpg",
-    price: 8.99,
-    category: "Read"
+    category: "Read",
   },
   {
     id: 3,
     title: "The Catcher in the Rye",
     author: "J.D. Salinger",
-    publisher: "Little, Brown and Company",
-    year: 1951,
     image:
       "https://m.media-amazon.com/images/I/51lprH3LF4L._SX323_BO1,204,203,200_.jpg",
-    price: 10.99,
-    category: "Currently Reading"
+    category: "Currently Reading",
   },
   {
     id: 4,
     title: "The Great Gatsby",
     author: "F. Scott Fitzgerald",
-    publisher: "Scribner",
-    year: 1925,
     image: "https://m.media-amazon.com/images/I/71FTb9X6wsL.jpg",
-    price: 7.99,
-    category: "Currently Reading"
+    category: "Currently Reading",
   },
   {
     id: 5,
     title: "Brave New World",
     author: "Aldous Huxley",
-    publisher: "Harper Perennial Modern Classics",
-    year: 1932,
     image:
       "https://pagdandi.org/wp-content/uploads/2021/04/Brave-New-World.jpg",
-    price: 12.99,
-    category: "Want To Read"
+    category: "Want To Read",
   },
   {
     id: 6,
     title: "Pride and Prejudice",
     author: "Jane Austen",
-    publisher: "Penguin Classics",
-    year: 1813,
     image: "https://m.media-amazon.com/images/I/71Q1tPupKjL.jpg",
-    price: 6.99,
-    category: "Want To Read"
+    category: "Want To Read",
   },
   {
     id: 7,
     title: "The Hobbit",
     author: "J.R.R. Tolkien",
-    publisher: "Stanley Unwin",
-    year: 1937,
-    image: "https://m.media-amazon.com/images/P/054792822X.01._SCLZZZZZZZ_SX500_.jpg",
-    price: 8.55,
-    category: "None"
+    image:
+      "https://m.media-amazon.com/images/P/054792822X.01._SCLZZZZZZZ_SX500_.jpg",
+    category: "None",
   },
   {
     id: 8,
     title: "To the Lighthouse",
     author: "Virginia Woolf",
-    publisher: "Hogarth Press",
-    year: 1927,
     image: "https://m.media-amazon.com/images/I/41GWqsNHAPL.jpg",
-    price: 8.55,
-    category: "None"
+    category: "None",
   },
 ];
